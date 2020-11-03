ccaff920e551b7f0e754d3a14a04419835b6a0e6
mockingrave <mockingrave@gmail.com>
2020-11-03 13:36:00 +0300

    added work with Date&Time

---
+++ src/main/java/myspring/DTO/DateTimeDTO.java
@@ -0,0 +1,22 @@
+package myspring.DTO;
+
+public class DateTimeDTO {
+    private String dateTime;
+    private String difference;
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String date) {
+        this.dateTime = date;
+    }
+
+    public String getDifference() {
+        return difference;
+    }
+
+    public void setDifference(String difference) {
+        this.difference = difference;
+    }
+}
